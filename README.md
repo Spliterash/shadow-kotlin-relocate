27367df8ee5e5a30cb870162dcf0d1ee8bcc019e
Spliterash <me@spliterash.ru>
2023-01-06 01:28:05 +0300

    Repo fix

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven {
-            url = uri("https://nexus.spliterash.ru/repository/gradle-plugin/")
+            url = uri("https://repo.spliterash.ru/group/")
         }
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ publishing {
     repositories {
         maven {
             name = "nexus"
-            url = uri("https://nexus.spliterash.ru/repository/" + rootProject.name)
+            url = uri("https://repo.spliterash.ru/" + rootProject.name)
             credentials {
                 username = findProperty("SPLITERASH_NEXUS_USR")?.toString()
                 password = findProperty("SPLITERASH_NEXUS_PSW")?.toString()
